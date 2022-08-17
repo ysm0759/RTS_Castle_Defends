56626f41789da2e9f6fcfef4735fc312eaaec6d3
YuSungMin <ysm0759@naver.com>
2022-08-17 11:38:07 +0900

    Fix : Camera Movement

---
+++ Assets/Script/Camera/CameraController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Screen Move")]
+    [SerializeField] float panSpeed = 20f;
+    [SerializeField] float panBorderThickness = 10f;
+    [SerializeField] Vector2 panLimitX;
+    [SerializeField] Vector2 panLimitZ;
+ 
+
+
+    [Header("Scoroll")]
+    [SerializeField] float scrollSpeed;
+    [SerializeField] float minY;
+    [SerializeField] float maxY;
+
+
+
+    Vector3 pos;
+
+
+    private void Update()
+    {
+
+        if (GameStateManager.instance.gameState != GameState.GAME_START)
+        {
+            return;
+        }
+
+        pos = transform.position;
+
+        CameraZoom();
+        CameraMove();
+        
+
+        
+
+
+        transform.position = pos;
+
+    }
+
+    void CameraZoom()
+    {
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        pos.y -= scroll * scrollSpeed *100f * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+    }
+
+    void CameraMove()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.mousePosition.x <= panBorderThickness)
+
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        pos.x = Mathf.Clamp(pos.x, panLimitX.x, panLimitX.y);
+        pos.z = Mathf.Clamp(pos.z, panLimitZ.x, panLimitZ.y);
+
+    }
+}
