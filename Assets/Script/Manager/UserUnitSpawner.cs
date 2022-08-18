4e7cdfa8bf9c32e88a3612828c44c9080e7c160a
YuSungMin <ysm0759@naver.com>
2022-08-18 17:20:39 +0900

    Fix : Unit Spawner

--- Assets/Script/Manager/UserUnitSpawner.cs
+++ Assets/Script/Manager/UserUnitSpawner.cs
@@ -11,6 +11,7 @@ public class UserUnitSpawner : MonoBehaviour
     [SerializeField]
     private List<GameObject> heroPrefab;
 
+    [SerializeField]
     private List<Transform> unitPrefabPosition;
 
     //오브젝트 풀링
@@ -35,6 +36,8 @@ public class UserUnitSpawner : MonoBehaviour
 
         Vector3 position = new Vector3(120, 5f, 120);
 
+
+
         for (int i =0; i < tmpData.Length; i++)
         {
 
@@ -42,14 +45,70 @@ public class UserUnitSpawner : MonoBehaviour
             {
                 continue;
             }
+
+            Vector3 tmpVec = unitPrefabPosition[i].position;
+            int dir = 0;
+            int max = 2;
+            int dirCnt = 0;
+            float place = 4f;
+
             for (int j = 0;  j < tmpData[i].population;j++)
             {
 
 
-                GameObject clone = Instantiate(UnitIndex[i].LevelObjects[tmpData[i].level-1], position, Quaternion.identity);
+                GameObject clone = Instantiate(UnitIndex[i].LevelObjects[tmpData[i].level - 1], position, Quaternion.identity);
                 clone.GetComponent<UnitInfo>().SetData(tmpData[i]);
                 UnitController unit = clone.GetComponent<UnitController>();
                 unitList.Add(unit);
+
+                if (0 == j)
+                {
+                    clone.transform.position = tmpVec;
+                    tmpVec.z += place;
+                    dirCnt = 1;
+                    continue;
+
+                }
+                else
+                {
+                    if (dirCnt == max)
+                    {
+                        dir++;
+                        dirCnt = 0;
+                        if (4 == dir)
+                        {
+                            clone.transform.position = tmpVec;
+
+                            max += 2;
+                            tmpVec.z += place;
+                            dirCnt = 1;
+                            dir = 0;
+                            continue;
+                        }
+                    }
+                    clone.transform.position = tmpVec;
+
+                    if (0 == dir)
+                    {
+                        tmpVec.x += place;
+                    }
+                    else if (1 == dir)
+                    {
+                        tmpVec.z -= place;
+                    }
+                    else if (2 == dir)
+                    {
+                        tmpVec.x -= place;
+                    }
+                    else if (3 == dir)
+                    {
+                        tmpVec.z += place;
+                    }
+
+                    dirCnt++;
+
+                }
+
             }
         }
 
@@ -73,8 +132,6 @@ public class UserUnitSpawner : MonoBehaviour
 
 
     }
-
-    
 
 
 }
